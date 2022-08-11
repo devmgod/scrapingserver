a29e427720ba01de9ae2ac9d1606f9d19d49c51c
Tykhon <richard.lee970112@gmail.com>
2022-08-12 08:30:17 +0900

    opimize

--- app.js
+++ app.js
@@ -55,16 +55,6 @@ const {
     upcomings,
 } = db;
 
-const allDbs = [
-    tamils,
-    featureds,
-    hindies,
-    kannadas,
-    malayalams,
-    telugues,
-    upcomings,
-];
-
 const videolink = {
     "https://www.tamilrockermovies.us/language/tamil/": tamils,
     "https://www.tamilrockermovies.us/featured/": featureds,
@@ -74,228 +64,257 @@ const videolink = {
     "https://www.tamilrockermovies.us/language/malayalam/": malayalams,
     "https://www.tamilrockermovies.us/language/telugu/": telugues,
 };
+const directory = "../tamilmovies/public/poster/";
 
-// const datafetch = () => {
-//   //directory format
-//   const directory = "../tamilmovies/public/poster/";
+const datafetch = async() => {
+    // directory format
 
-//   fs.readdir(directory, (err, files) => {
-//     if (err) throw err;
+    Object.keys(videolink).forEach(async(key) => {
+        //Scraping
+        await scraping(key);
+    });
+};
 
-//     for (const file of files) {
-//       fs.unlink(path.join(directory, file), (err) => {
-//         if (err) throw err;
-//       });
-//     }
-//   });
+const scraping = async(key) => {
+    try {
 
-//   allDbs.map((val) => {
-//     val.destroy({
-//       where: {},
-//       truncate: false,
-//     });
-//   });
+        const document = await fetchFun(key);
 
-//   Object.keys(videolink).map((key) => {
-//     //Scraping
-//     fetch(key)
-//       .then((res) => res.text())
-//       .then((text) => {
-//         const dom = new jsdom.JSDOM(text);
-//         const document = dom.window.document;
+        //scrap function
+        await scrapingContent(document, key);
 
-//         //scrap function
-//         scrapingContent(document);
+        //page number
+        const allPage = document
+            .getElementsByTagName("nav")[2]
+            .getElementsByTagName("span")[0].innerHTML;
 
-//         //page number
-//         const allPage = document
-//           .getElementsByTagName("nav")[2]
-//           .getElementsByTagName("span")[0].innerHTML;
+        const pageNum = allPage.split(" ")[3];
 
-//         const pageNum = allPage.split(" ")[3];
 
-//         // all pages fetch
-//         for (let i = 2; i <= pageNum; i++) {
-//           //node-fetch
-//           const url_Num = key + "page/" + i + "/";
-//           fetch(url_Num)
-//             .then((res) => res.text())
-//             .then((text) => {
-//               const dom = new jsdom.JSDOM(text);
-//               const document = dom.window.document;
+        // all pages fetch
+        for (let i = 2; i <= pageNum; i++) {
+            //node-fetch
+            const url_Num = key + "page/" + i + "/";
+            const document2 = await fetchFun(url_Num);
 
-//               //scrap function
-//               scrapingContent(document);
-//             });
-//         }
-//       });
-//     const scrapingContent = (document) => {
-//       const movieData = Array.from(document.getElementsByTagName("figure")).map(
-//         (e) => [
-//           e.getElementsByTagName("a")[0].href,
-//           // e.getElementsByTagName("div")[0].style.backgroundImage,
-//         ]
-//       );
+            //scrap function
+            await scrapingContent(document2, key);
+        }
+    } catch (e) {}
 
-//       //sub data fetch
-//       movieData.map((val, index) => {
-//         fetch(val)
-//           .then((res) => res.text())
-//           .then((text) => {
-//             // fs.writeFileSync("./test-sync.txt", text);
-//             const dom = new jsdom.JSDOM(text);
-//             const document = dom.window.document;
 
-//             const poster = JSON.parse(
-//               document.querySelectorAll("[type = 'application/ld+json']")[1]
-//                 .innerHTML
-//             ).itemListElement[2].item.image;
-//             const year = document
-//               .getElementsByClassName("movie-info")[0]
-//               .getElementsByTagName("a")[0].innerHTML;
-//             const genres = document
-//               .getElementsByClassName("movie-info")[0]
-//               .getElementsByTagName("a")[1].innerHTML;
-//             const country = document
-//               .getElementsByClassName("movie-info")[0]
-//               .getElementsByTagName("a")[2].innerHTML;
-//             const language = document
-//               .getElementsByClassName("movie-info")[0]
-//               .getElementsByTagName("a")[3].innerHTML;
-//             const runtime = document
-//               .getElementsByClassName("movie-info")[0]
-//               .getElementsByTagName("li")[1]
-//               .innerHTML.split(":")[1];
-//             const title = document
-//               .getElementsByTagName("header")[1]
-//               .getElementsByTagName("h1")[0].innerHTML;
-//             const videourl = document
-//               .getElementsByTagName("header")[3]
-//               .getElementsByTagName("a")[0].href;
+}
 
-//             // download image
-//             const url = poster;
-//             const iName = poster.split("/");
-//             const imgName = iName[iName.length - 1];
 
-//             const path = "../tamilmovies/public/poster/" + imgName;
-//             download(url, path, () => {
-//               console.log("1 ✅ Done!");
-//             });
+const scrapingContent = async(document, key) => {
+    const movieData = Array.from(document.getElementsByTagName("figure")).map(
+        (e) => [
+            e.getElementsByTagName("a")[0].href
+        ]
+    );
+    const downloadUrls = new Set(); //unique value array
+    //sub data fetch
 
-//             const movieInfor = {
-//               poster: imgName,
-//               title: title.trim(),
-//               year: year,
-//               genres: genres,
-//               country: country,
-//               language: language,
-//               runtime: runtime,
-//               videourl: videourl,
-//             };
+    movieData.map(async(e, index) => {
+        const document = await fetchFun(e)
 
-//             //create db
-//             allDbs.map((val) => {
-//               if (videolink[key] == val) {
-//                 val.create(movieInfor);
-//               }
-//             });
-//           });
-//       });
-//     };
-//     const download = (url, path, callback) => {
-//       request.head(url, (err, res, body) => {
-//         request(url).pipe(fs.createWriteStream(path)).on("close", callback);
-//       });
-//     };
-//   });
-// };
+        try {
+
+            const poster =
+                document.querySelectorAll(
+                    "[type = 'application/ld+json']"
+                )[1] &&
+                JSON.parse(
+                    document.querySelectorAll("[type = 'application/ld+json']")[1]
+                    .innerHTML
+                ).itemListElement[2].item.image;
+            const year = document
+                .getElementsByClassName("movie-info")[0]
+                .getElementsByTagName("a")[0].innerHTML;
+            const genres = document
+                .getElementsByClassName("movie-info")[0]
+                .getElementsByTagName("a")[1].innerHTML;
+            const country = document
+                .getElementsByClassName("movie-info")[0]
+                .getElementsByTagName("a")[2].innerHTML;
+            const language = document
+                .getElementsByClassName("movie-info")[0]
+                .getElementsByTagName("a")[3].innerHTML;
+            const runtime = document
+                .getElementsByClassName("movie-info")[0]
+                .getElementsByTagName("li")[1]
+                .innerHTML.split(":")[1];
+            const title = document
+                .getElementsByTagName("header")[1]
+                .getElementsByTagName("h1")[0].innerHTML;
+            const videourl = document
+                .getElementsByTagName("header")[3]
+                .getElementsByTagName("a")[0].href;
+
+            //create db
+            const myDb = videolink[key];
+            const tbData = await myDb.findAll({ where: null });
+            const titles = tbData.map((e) => e.title);
+            if (titles.includes(title.trim())) {
+
+                return;
+            } else {
+
+                // download image
+                const url = poster;
+                const iName = poster.split("/");
+                const imgName = iName[iName.length - 1];
+
+                const path = directory + imgName;
+                console.log(index, 'wwwwwwwwwwwwwwwww', path)
+                    // const files = fs.readdirSync(directory);
+                await new Promise((resolve) => {
+                    if (downloadUrls.has(url)) {
+                        resolve();
+                        return;
+                    }
+                    download(url, path, () => {
+                        console.log("1 ✅ Done!");
+                        downloadUrls.add(url);
+                        resolve();
+                    });
+                });
+
+                const movieInfor = {
+                    poster: imgName,
+                    title: title.trim(),
+                    year: year,
+                    genres: genres,
+                    country: country,
+                    language: language,
+                    runtime: runtime,
+                    videourl: videourl,
+                };
+                console.log("not", title);
+                await myDb.create(movieInfor);
+            }
+
+        } catch (e) {}
+    });
+};
+const download = (url, path, callback) => {
+    request.head(url, (err, res, body) => {
+        request(url).pipe(fs.createWriteStream(path)).on("close", callback);
+    });
+};
+
+const fetchFun = async(key) => {
+    const res = await fetch(key)
+    const text = await res.text()
+    const dom = new jsdom.JSDOM(text);
+    const document = dom.window.document;
+    return document;
+}
 
 const doodApi = () => {
-    allDbs.map(async(dbSpecific) => {
-        const tbData = await dbSpecific.findAll({ where: null });
+    Object.keys(videolink).forEach(async(dbSpecific) => {
+        const tbData = await videolink[dbSpecific].findAll({ where: null });
         tbData.map((val, index) => {
             // console.log(val.title);
-            // axios
-            //     .get(
-            //         "https://doodapi.com/api/file/list?key=131197ck2gd3ei6kkw08j0&page=1&per_page=10000"
-            //     )
-            //     .then((res) => {
-            //         res.data.result.files.map((list) => {
-            //             if (val.filecode == list.file_code) {
-            //                 console.log(
-            //                     index,
-            //                     `----------------------------`,
-            //                     list.file_code,
-            //                     list.download_url,
-            //                     val.title,
-            //                     val.videourl
-            //                 );
-            //                 // update file_code
-            //                 const movieInfor = {
-            //                     iframurl: list.download_url,
-            //                 };
-            //                 dbSpecific.update(movieInfor, {
-            //                     where: { filecode: list.file_code, updatedAt: null, },
-            //                 });
-            //             }
-            //         });
-            //     })
-            //     .catch((error) => {
-            //         //   console.error(error);
-            //     });
-
             axios
                 .get(
-                    "https://doodapi.com/api/urlupload/actions?key=131197ck2gd3ei6kkw08j0&clear_errors=yes"
+                    "https://doodapi.com/api/file/list?key=131197ck2gd3ei6kkw08j0&page=1&per_page=10000"
                 )
                 .then((res) => {
-                    console.log(`statusCode: ${res.data.msg}`);
+                    res.data.result.files.map((list) => {
+                        if (val.filecode == list.file_code) {
+                            console.log(
+                                index,
+                                `----------------------------`,
+                                list.file_code,
+                                list.download_url,
+                                val.title,
+                                val.videourl
+                            );
+                            // update file_code
+                            const movieInfor = {
+                                iframurl: list.download_url,
+                            };
+                            videolink[dbSpecific].update(movieInfor, {
+                                where: { filecode: list.file_code },
+                            });
+                        }
+                    });
                 })
                 .catch((error) => {
                     //   console.error(error);
                 });
-            axios
-                .get(
-                    "https://doodapi.com/api/urlupload/slots?key=131197ck2gd3ei6kkw08j0"
-                )
-                .then((res) => {
-                    if (res.data.used_slots < 100) {
-                        axios
-                            .get(
-                                "https://doodapi.com/api/upload/url?key=131197ck2gd3ei6kkw08j0&url=" +
-                                val.videourl +
-                                "&new_title=" +
-                                val.title
-                            )
-                            .then((res) => {
-                                console.log(`statusCode: ${res.status}`);
 
-                                //update file_code
-                                const movieInfor = {
-                                    filecode: res.data.file_code,
-                                };
-                                dbSpecific.update(movieInfor, {
-                                    where: { title: val.title },
-                                });
-                            })
-                            .catch((error) => {
-                                console.error(error);
-                            });
-                    } else if (res.data.used_slots >= 100) {
-                        return;
-                    }
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
+            // axios
+            //     .get(
+            //         "https://doodapi.com/api/urlupload/actions?key=131197ck2gd3ei6kkw08j0&clear_errors=yes"
+            //     )
+            //     .then((res) => {
+            //         console.log(`statusCode: ${res.data.msg}`);
+            //     })
+            //     .catch((error) => {
+            //         //   console.error(error);
+            //     });
+            // axios
+            //     .get(
+            //         "https://doodapi.com/api/urlupload/slots?key=131197ck2gd3ei6kkw08j0"
+            //     )
+            //     .then((res) => {
+            //         if (res.data.used_slots < 100) {
+            //             axios
+            //                 .get(
+            //                     "https://doodapi.com/api/upload/url?key=131197ck2gd3ei6kkw08j0&url=" +
+            //                     val.videourl +
+            //                     "&new_title=" +
+            //                     val.title
+            //                 )
+            //                 .then((res) => {
+            //                     console.log(`statusCode: ${res.status}`);
+
+            //                     //update file_code
+            //                     const movieInfor = {
+            //                         filecode: res.data.file_code,
+            //                     };
+            //                     videolink[dbSpecific].update(movieInfor, {
+            //                         where: { title: val.title },
+            //                     });
+            //                 })
+            //                 .catch((error) => {
+            //                     console.error(error);
+            //                 });
+            //         } else if (res.data.used_slots >= 100) {
+            //             return;
+            //         }
+            //     })
+            //     .catch((error) => {
+            //         console.error(error);
+            //     });
         });
     });
 };
 
-doodApi();
+const db_dir_format = () => {
+    // fs.readdir(directory, (err, files) => {
+    //     if (err) throw err;
 
-// datafetch();
+    //     for (const file of files) {
+    //         fs.unlink(path.join(directory, file), (err) => {
+    //             if (err) throw err;
+    //         });
+    //     }
+    // });
+
+    // allDbs.map((val) => {
+    //   val.destroy({
+    //     where: {},
+    //     truncate: false,
+    //   });
+    // });
+}
+
+// doodApi();
+
+datafetch();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
